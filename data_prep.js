c912eaeb43a68a8e4386453dcda3a7b91334e44a
mshaikh51 <111617732+mshaikh51@users.noreply.github.com>
2022-11-24 14:27:05 -0500

    test 5

--- data_prep.js
+++ data_prep.js
@@ -1,176 +1,154 @@
+const Sequelize = require("sequelize");
+var sequelize = new Sequelize(
+  "psqxicbk",
+  "psqxicbk",
+  "hB1oIZlVwbGA4jvuszwyyhm-UJrEGeFz",
+  {
+    host: "heffalump.db.elephantsql.com",
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
+    query: { raw: true },
+  }
+);
+
+var Student = sequelize.define("Student", {
+  StudId: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: Sequelize.STRING,
+  program: Sequelize.STRING,
+  gpa: Sequelize.FLOAT,
+});
+
 var fs = require("fs");
 
-var students=[];
-
-exports.prep = ()=>{
-
-   // console.log("Testing");
-
-   return new Promise((resolve, reject)=>{
-
-        fs.readFile("./students.json", (err, data)=>{
-
-            if (err) {reject("unable to read file.");}
-
-            students = JSON.parse(data);
-
-           // console.log(students);
-
-            resolve("File read success.");
-
-        }); 
-
-   });
-
-};
-
-
-
-exports.bsd = ()=>{
-
-    return new Promise((resolve, reject)=>{
-
-       let results = students.filter(student => student.program == "BSD");
-
-       (results.length == 0)? reject("No BSD students."):resolve(results);
-
-    });
-
-}
-
-
-
-
-
-exports.cpa = ()=>{
-
-    return new Promise((resolve, reject)=>{
-
-       let results = students.filter(student => student.program == "CPA");
-
-       (results.length == 0)? reject("No CPA students."):resolve(results);
-
-    });
-
-}
-
-exports.highGPA = ()=>{
-
-    return new Promise((resolve, reject)=>{
-
-        let high = 0;
-
-        let highStudent;
-
-        
-
-        for (let i=0; i<students.length; i++)
-
-        {
-
-            //console.log(students[i].gpa, high);
-
-            if (students[i].gpa > high)
-
-            {
-
-                high = students[i].gpa;
-
-                highStudent = students[i];
-
-            }
-
-        }
-
-        (highStudent) ? resolve(highStudent): reject("Failed finding student with highest GPA");
-
-    }); 
-
-};
-
-
-
-exports.lowGPA = ()=>{
-
-    return new Promise((resolve, reject)=>{
-
-        let low = 4.0;
-
-        let lowStudent;
-
-        for (let i=0; i<students.length; i++)
-
-        {
-
-            if (students[i].gpa < low)
-
-            {
-
-                low = students[i].gpa;
-
-                lowStudent = students[i];
-
-            }
-
-        }
-
-        resolve(lowStudent);
-
-    }); 
-
-};
-
-
-
-exports.allStudents =()=>{
-
-    return new Promise((resolve, reject)=>{
-
-        if (students.length>0)
-
-        {
-
-            resolve(students);
-
-        } else reject("No students.");
-
-    })
-
-}
-
-
-
-exports.addStudent= (stud)=>{
-
-    return new Promise((resolve, reject)=>{
-
-        stud.studId = students.length+1;
-
-        students.push(stud);
-
+var students = [];
+
+exports.prep = () => {
+  return new Promise((resolve, reject) => {
+    sequelize
+      .sync()
+      .then(function () {
         resolve();
+      })
+      .catch(function (params) {
+        reject("unable to sync the database");
+      });
+  });
+};
 
+exports.addStudent = (stud) => {
+  return new Promise((resolve, reject) => {
+    Student.create(stud)
+      .then(() => {
+        resolve();
+      })
+      .catch(() => {
+        reject("unable to add the student");
+      });
+  });
+};
+
+exports.cpa = () => {
+  return new Promise((resolve, reject) => {
+    Student.findAll({ where: { program: "CPA" } })
+      .then((data) => {
+      //  console.log("cpa");
+        //console.log(data);
+        resolve(data);
+      }).catch(function () {
+        
+      reject("no results returned");
+      })
+
+  });
+};
+
+exports.highGPA = () => {
+  return new Promise((resolve, reject) => {
+    Student.findAll({
+    }).then(function (data) {
+      let high = 0;
+
+      let highStudent;
+
+      let students=data;
+      for (let i=0; i<students.length; i++)
+
+      {
+
+          //console.log(students[i].gpa, high);
+
+          if (students[i].gpa > high)
+
+          {
+
+              high = students[i].gpa;
+
+              highStudent = students[i];
+
+          }
+
+      }
+
+      (highStudent) ? resolve(highStudent): reject("Failed finding student with highest GPA");
+      }).catch(function () {
+        reject("no results returned");
+      });
+  });
+};
+exports.allStudents = () => {
+  return new Promise((resolve, reject) => {
+    Student.findAll()
+      .then(function (data) {
+        resolve(data);
+      })
+      .catch(function () {
+        reject("no results returned");
+      });
+  });
+};
+/*
+exports.bsd = () => {
+  return new Promise((resolve, reject) => {
+    let results = students.filter((student) => student.program == "BSD");
+
+    results.length == 0 ? reject("No BSD students.") : resolve(results);
+  });
+};
+
+exports.lowGPA = () => {
+  return new Promise((resolve, reject) => {
+    let low = 4.0;
+
+    let lowStudent;
+
+    for (let i = 0; i < students.length; i++) {
+      if (students[i].gpa < low) {
+        low = students[i].gpa;
+
+        lowStudent = students[i];
+      }
+    }
+
+    resolve(lowStudent);
+  });
+};
+
+
+
+exports.getStudent = (studId) => {
+  return new Promise((resolve, reject) => {
+    students.forEach(function (student) {
+      if (student.studId == studId) resolve(student);
     });
 
-}
-
-
-
-exports.getStudent = (studId)=>{
-
-    return new Promise((resolve, reject)=>{
-
-        students.forEach(function(student){
-
-            if (student.studId == studId)
-
-                resolve(student);
-
-        });
-
-        reject("No result found!");
-
-    })
-
-}
-
+    reject("No result found!");
+  });
+};
+*/
--- server.js
+++ server.js
@@ -1,145 +1,89 @@
-//https://github.com/mshaikh51/test4
-//https://important-foal-tam.cyclic.app
 var express = require("express");
-
 var app = express();
-
 var data_prep = require("./data_prep.js");
-
 var path = require("path");
+const {engine} = require("express-handlebars");
 
-var exphbs = require("express-handlebars");
-app.engine('.hbs', exphbs.engine({
-    extname: '.hbs'
-}));
-app.set('view engine', '.hbs');
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.use(express.urlencoded({ extended: true }));
-
-
+app.engine(".hbs", engine({
+        extname: '.hbs',
+        defaultLayout: 'main'
+    }
+));
+app.set("view engine", ".hbs");
 
 var HTTP_PORT = process.env.PORT || 8080;
-
-function onHttpStart() {
-
+function onHttpStart() 
+{
     console.log("Express http server listening on " + HTTP_PORT);
-
 }
 
-
-
-app.get("/", (req, res) => {
-        res.render("home");
-
+app.get("/",(req,res)=>{
+    res.render("home");
 });
 
-
-
-app.get("/BSD", (req, res) => {
-
-    data_prep.bsd().then((data) => {
-
-        res.json(data);
-
-    }).catch((reason) => {
-
-        res.json({ message: reason });
-
+app.get("/BSD", (req,res)=>{
+    data_prep.bsd().then((data)=>{
+        res.render("students",{data: data});
+    }).catch((reason)=>{
+        res.json({message:reason});
     });
-
 });
 
-
-
-app.get("/CPA", (req, res) => {
-
-    data_prep.cpa().then((mdata) => {
-        res.render("students",{data:mdata});
-
-    }).catch((reason) => {
-
-        res.json({ message: reason });
-
+app.get("/CPA", (req,res)=>{
+    data_prep.cpa().then((data)=>{
+        console.log("rendering");
+        console.log(data);
+        res.render("students",{data: data});
+    }).catch((reason)=>{
+        res.json({message:reason});
     });
-
 });
 
-
-
-app.get("/highGPA", (req, res) => {
-
-    data_prep.highGPA().then((mdata) => {
-res.render("student",{data:mdata});
+app.get("/highGPA", (req, res)=>{
+    data_prep.highGPA().then((data)=>{
+    res.render("student",{data:data});
+    }).catch(function (data) {
+        res.json({message:data});
     });
-
 });
 
-
-
-app.get("/allStudents", (req, res) => {
-
-    data_prep.allStudents().then((mdata) => {
-        
-        res.render("students", {data:mdata},);
-
-    }).catch((reason) => res.json({ message: reason }));
-
+app.get("/allStudents", (req, res)=>{
+    data_prep.allStudents().then((data)=>{
+        res.render("students",{data: data});
+    }).catch((reason)=>res.json({message:reason}));
 });
 
-
-
-app.get("/addStudent", (req, res) => {
-
-    res.sendFile(path.join(__dirname, "/test3_views/addStudent.html"));
-
+app.get("/addStudent", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/views/addStudent.html"));
 });
 
+app.post("/addStudent", (req, res)=>{
+    data_prep.addStudent(req.body).then(()=>{
+        var data = req.body;
+        res.render("student",{data:data});
+        //res.redirect("/allStudents");
 
-
-app.post("/addStudent", (req, res) => {
-
-    data_prep.addStudent(req.body).then(() => {
-
-        var mdata = req.body;
-        res.render("student",{data:mdata});
-
-    }).catch((reason) => res.json({ message: reason }));
-
+    }).catch((reason)=>res.json({message:reason}));
 });
 
-
-
-app.get("/student/:studId", (req, res) => {
-
-    data_prep.getStudent(req.params.studId).then((mdata) => {
-
-        res.render("student",{data:mdata});
-
-
-        
-        }).catch((reason) => res.json({ message: reason }));
-
+app.get("/student/:studId",(req, res)=>{
+    data_prep.getStudent(req.params.studId).then((data)=>{
+        res.render("student", {data:data});
+       // res.json(data);
+       // {"studId":3,"name":"name3","program":"BSD","gpa":3.3}
+    }).catch((reason)=>res.json({message:reason}));
 });
 
-
-
-app.get("*", (req, res) => {
-
+app.get("*", (req, res)=>{
     res.status(404).send("Error 404: page not found.")
-
 });
 
-
-
-data_prep.prep().then((data) => {
-
+data_prep.prep().then((data)=>{
     //console.log(data);
-
     app.listen(HTTP_PORT, onHttpStart);
-
-}).catch((err) => {
-
+}).catch((err)=>{
     console.log(err);
-
 });
